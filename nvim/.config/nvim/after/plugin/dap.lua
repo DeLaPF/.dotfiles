d57cefd7dee6f8f76017691d653f73bbddcc201f
DeLaPF <de.alafia@gmail.com>
2024-08-24 09:24:21 +0000

    cleanup dap, still some hackiness (good reference commit if want to fix later)

--- nvim/.config/nvim/after/plugin/dap.lua
+++ nvim/.config/nvim/after/plugin/dap.lua
@@ -23,16 +23,11 @@ local function get_buf_tail()
     if not buf_name then
         return nil
     end
-    -- return buf_name:match'[^\\/]+$'
     return string.match(buf_name, '[^\\/]+$')
 end
 
 vim.keymap.set('n', 'p', function()
     local buf_tail = get_buf_tail()
-    -- local buf_tail = vim.fn.expand(
-    --     vim.fn.escape(vim.api.nvim_buf_get_name(0), '[]') .. ':t'
-    -- )
-    -- print(buf_tail)
     if not buf_tail then
         return 'p'
     end
@@ -42,16 +37,6 @@ vim.keymap.set('n', 'p', function()
         return 'p'
     end
 
-    -- I would like for this to work
-    -- require'dap'.repl.execute(vim.fn.getreg('"'))
-    -- But it is getting the same error regardles of how I implement
-    -- local line = vim.fn.getreg('"')
-    -- local lnum = vim.fn.line('$')
-    -- line = string.gsub(line, '\n', '')
-    -- vim.api.nvim_buf_set_lines(0, lnum - 1, lnum, true, {'dap> ' .. line})
-    -- vim.cmd('startinsert!')
-    -- So here's a hacky way
-    -- local first_line = vim.fn.getreg('"')
     local first_line = vim.split(vim.fn.getreg('"'), '\n')[1]
     if not first_line then
         return ''
@@ -60,26 +45,16 @@ vim.keymap.set('n', 'p', function()
 end, { expr = true })
 
 -- Auto open/close dapui
-dap.listeners.before.attach.dapui_config = function()
-    ui.open()
-end
-dap.listeners.before.launch.dapui_config = function()
-    ui.open()
-end
-dap.listeners.before.event_terminated.dapui_config = function()
-    ui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-    ui.close()
-end
+dap.listeners.before.attach.dapui_config = ui.open
+dap.listeners.before.launch.dapui_config = ui.open
+dap.listeners.before.event_terminated.dapui_config = ui.close
+dap.listeners.before.event_exited.dapui_config = ui.close
 
 dap.adapters.debugpy = {
     type = 'executable',
     command = 'python',
     args = { '-m', 'debugpy.adapter' },
 }
-
--- local dap_utils = require('utils.dap')
 
 -- Python
 require('dap-python').setup('python')
