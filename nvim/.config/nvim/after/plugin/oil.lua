d247c7814c08c7663495d940d3be5581d7fe038c
DeLaPF <de.alafia@gmail.com>
2024-08-24 21:34:20 +0000

    clean up oil

--- nvim/.config/nvim/after/plugin/oil.lua
+++ nvim/.config/nvim/after/plugin/oil.lua
@@ -1,11 +1,12 @@
 require('oil').setup({
-    default_file_explorer = false,
+    default_file_explorer = true,
     view_options = {
         show_hidden = true,
     },
+    keymaps = {
+        ['q'] = 'actions.close',
+    }
 })
 
--- vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 -- using floating avoids adding to jump list
-vim.keymap.set("n", "-", require("oil").toggle_float)
-vim.keymap.set('n', '<leader><leader>q', require('oil').close)
+vim.keymap.set('n', '-', require('oil').toggle_float)
