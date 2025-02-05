db021846ff5042c7e820ec8ebeb3eb036a7e4824
DeLaPF <de.alafia@gmail.com>
2025-02-05 00:22:58 -0500

    It appears that the yank script is not required for copying, that is instead handled by [["+y]] (thank you asbjornHaland), though I'm not sure how that is being passed through even ssh??? (invisible osc52?)
    Also reverted to normal use of arrow keys, at the expense of having to use tmux prefix before scrolling up

--- nvim/.config/nvim/lua/delapf/init.lua
+++ nvim/.config/nvim/lua/delapf/init.lua
@@ -28,15 +28,3 @@ if vim.fn.empty(additional_dirs) ~= 1 then
         end
     end
 end
-
--- TODO: is there a way to make work without making global?
--- function Yank(text)
---     local escape = vim.fn.system('yank.sh', text)
---     if vim.v.shell_error ~= 0 then
---         error(escape)
---     else
---         vim.fn.writefile({escape}, '/dev/tty', 'b')
---     end
--- end
--- vim.keymap.set('n', '<leader>y', 'y:lua Yank(@0)<CR>')
--- TODO: is it possible to open a buffer, write the escape sequence, and then close it to copy to clipboard?
