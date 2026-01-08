44fb69266f9f7e957d252efc54b0fc280571ed09
DeLaPF <de.alafia@gmail.com>
2026-01-08 00:26:37 -0500

    fix treesitter v1.0 issue breaking config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,9 @@
 return {  -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
+    version = false,
     build = ":TSUpdate",
     main = "nvim-treesitter.configs", -- Sets main module to use for opts
+    branch = "master",
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
         ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline", "query", "vim", "vimdoc" },
